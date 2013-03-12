fd9028ab57f0da40a9494a46512481db66dbc608
ppedrot <ppedrot@0cf17b13-060f-0410-b1b1-c666bec9822a>
2013-03-12 20:26:23 +0000

    Fix Random for Local Definitions.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/coq-contribs/trunk@2265 0cf17b13-060f-0410-b1b1-c666bec9822a

--- Ubase.v
+++ Ubase.v
@@ -32,75 +32,75 @@ Notation "[1/]1+ n" := (Unth n) (at level 35, right associativity) : U_scope.
 Open Local Scope U_scope.
 
 (** ** Basic Properties *)
-Hypothesis Ueq_refl : forall x:U, x == x.
-Hypothesis Ueq_sym : forall x y:U, x == y -> y == x.
+Axiom Ueq_refl : forall x:U, x == x.
+Axiom Ueq_sym : forall x y:U, x == y -> y == x.
 
-Hypothesis Udiff_0_1 : ~0 == 1.
-Hypothesis Upos : forall x:U, 0 <= x.
-Hypothesis Unit : forall x:U, x <= 1.
+Axiom Udiff_0_1 : ~0 == 1.
+Axiom Upos : forall x:U, 0 <= x.
+Axiom Unit : forall x:U, x <= 1.
 
-Hypothesis Uplus_sym : forall x y:U, x + y == y + x.
-Hypothesis Uplus_assoc : forall x y z:U, x + (y + z) == x + y + z.
-Hypothesis Uplus_zero_left : forall x:U, 0 + x == x.
+Axiom Uplus_sym : forall x y:U, x + y == y + x.
+Axiom Uplus_assoc : forall x y z:U, x + (y + z) == x + y + z.
+Axiom Uplus_zero_left : forall x:U, 0 + x == x.
 
-Hypothesis Umult_sym : forall x y:U, x * y == y * x.
-Hypothesis Umult_assoc : forall x y z:U, x * (y * z) == x * y * z.
-Hypothesis Umult_one_left : forall x:U, 1 * x == x.
+Axiom Umult_sym : forall x y:U, x * y == y * x.
+Axiom Umult_assoc : forall x y z:U, x * (y * z) == x * y * z.
+Axiom Umult_one_left : forall x:U, 1 * x == x.
 
-Hypothesis Uinv_one : [1-] 1 == 0. 
-Hypothesis Uinv_opp_left : forall x, [1-] x + x == 1.
+Axiom Uinv_one : [1-] 1 == 0. 
+Axiom Uinv_opp_left : forall x, [1-] x + x == 1.
 
 (** Property  : $1 - (x+y) + x=1-y$ holds when $x+y$ does not overflow *)
-Hypothesis Uinv_plus_left : forall x y, y <= [1-] x -> [1-] (x + y) + x == [1-] y.
+Axiom Uinv_plus_left : forall x y, y <= [1-] x -> [1-] (x + y) + x == [1-] y.
 
 (** Property  : $(x + y) \times z  = x \times z + y \times z$ 
     holds when $x+y$ does not overflow *)
-Hypothesis Udistr_plus_right : forall x y z, x <= [1-] y -> (x + y) * z == x * z + y * z.
+Axiom Udistr_plus_right : forall x y z, x <= [1-] y -> (x + y) * z == x * z + y * z.
 
 (** Property  : $1 - (x \times y) = (1 - x)\times y + (1-y)$ *)
-Hypothesis Udistr_inv_right : forall x y:U,  [1-] (x * y) == ([1-] x) * y + [1-] y.
+Axiom Udistr_inv_right : forall x y:U,  [1-] (x * y) == ([1-] x) * y + [1-] y.
 
 (** The relation $x\leq y$ is reflexive, transitive and anti-symmetric *)
-Hypothesis Ueq_le : forall x y:U, x == y -> x <= y.
+Axiom Ueq_le : forall x y:U, x == y -> x <= y.
 
-Hypothesis Ule_trans : forall x y z:U, x <= y -> y <= z -> x <= z.
+Axiom Ule_trans : forall x y z:U, x <= y -> y <= z -> x <= z.
 
-Hypothesis Ule_antisym : forall x y:U, x <= y -> y <= x -> x == y.
+Axiom Ule_antisym : forall x y:U, x <= y -> y <= x -> x == y.
 
 (** Totality of the order *)
-Hypothesis Ule_class : forall x y : U, class (x <= y).
+Axiom Ule_class : forall x y : U, class (x <= y).
 
-Hypothesis Ule_total : forall x y : U, orc (x<=y) (y<=x).
+Axiom Ule_total : forall x y : U, orc (x<=y) (y<=x).
 Implicit Arguments Ule_total [].
 
 (** The relation $x\leq y$ is compatible with operators *)
-Hypothesis Uplus_le_compat_left : forall x y z:U, x <= y -> x + z <= y + z.
+Axiom Uplus_le_compat_left : forall x y z:U, x <= y -> x + z <= y + z.
 
-Hypothesis Umult_le_compat_left : forall x y z:U, x <= y -> x * z <= y * z.
+Axiom Umult_le_compat_left : forall x y z:U, x <= y -> x * z <= y * z.
 
-Hypothesis Uinv_le_compat : forall x y:U, x <= y -> [1-] y <= [1-] x.
+Axiom Uinv_le_compat : forall x y:U, x <= y -> [1-] y <= [1-] x.
 
 (** Properties of simplification in case there is no overflow *)
-Hypothesis Uplus_le_simpl_right : forall x y z, z <= [1-] x -> x + z <= y + z -> x <= y.
+Axiom Uplus_le_simpl_right : forall x y z, z <= [1-] x -> x + z <= y + z -> x <= y.
 
-Hypothesis Umult_le_simpl_left : forall x y z: U, ~0 == z -> z * x <= z * y -> x <= y .
+Axiom Umult_le_simpl_left : forall x y z: U, ~0 == z -> z * x <= z * y -> x <= y .
 
 (** Property [Unth] $\frac{1}{n+1} == 1 - n \times \frac{1}{n+1}$ *)
-Hypothesis Unth_prop : forall n, [1/]1+n == [1-](comp Uplus 0 (fun k => [1/]1+n) n).
+Axiom Unth_prop : forall n, [1/]1+n == [1-](comp Uplus 0 (fun k => [1/]1+n) n).
 
 (** Archimedian property *)
-Hypothesis archimedian : forall x, ~0 == x -> exc (fun n => [1/]1+n <= x).
+Axiom archimedian : forall x, ~0 == x -> exc (fun n => [1/]1+n <= x).
 
 (** ** Least upper bound, corresponds to limit for increasing sequences *)
-Variable lub : (nat -> U) -> U.
+Parameter lub : (nat -> U) -> U.
 
-Hypothesis le_lub : forall (f : nat -> U) (n:nat), f n <= lub f.
-Hypothesis lub_le : forall (f : nat -> U) (x:U), (forall n, f n <= x) -> lub f <= x.
+Axiom le_lub : forall (f : nat -> U) (n:nat), f n <= lub f.
+Axiom lub_le : forall (f : nat -> U) (x:U), (forall n, f n <= x) -> lub f <= x.
 
 (** Stability properties of lubs with respect to [+] and [*] *)
-Hypothesis lub_eq_plus_cte_right : forall (f:nat->U) (k:U), lub (fun n => (f n) + k) == (lub f) + k.
+Axiom lub_eq_plus_cte_right : forall (f:nat->U) (k:U), lub (fun n => (f n) + k) == (lub f) + k.
 
-Hypothesis lub_eq_mult : forall (k:U) (f:nat->U), lub (fun n => k * (f n)) ==  k * lub f.
+Axiom lub_eq_mult : forall (k:U) (f:nat->U), lub (fun n => k * (f n)) ==  k * lub f.
 
 End Universe.
 
